d8c5adaa54cb48cfd7ea74c1d5bd8f35a59f019c
Alexander Tschantz <tschantz.alec@gmail.com>
2025-02-22 15:46:59 +0000

    example notebook

--- rssm/model.py
+++ rssm/model.py
@@ -1,4 +1,4 @@
-from typing import Tuple, NamedTuple
+from typing import Tuple, NamedTuple, Optional
 
 import equinox as eqx
 from jax import Array, numpy as jnp, random as jr, nn, lax, vmap
@@ -41,15 +41,15 @@ class Prior(eqx.Module):
         )
 
     def __call__(self, state: State, action: Array, key: jr.PRNGKey) -> State:
-        inp = jnp.concatenate([action, state.stoch], axis=-1)  # (B, A + S)
-        hidden = nn.elu(self.input_proj(inp))  # (B, E)
+        inp = jnp.concatenate([action, state.stoch], axis=-1)
+        hidden = nn.elu(self.input_proj(inp))
 
-        new_deter = self.cell(hidden, state.deter)  # (B, D)
+        new_deter = self.cell(hidden, state.deter)
 
-        out = nn.elu(self.stoch_proj1(new_deter))  # (B, H)
-        out = self.stoch_proj2(out)  # (B, 2 * S)
+        out = nn.elu(self.stoch_proj1(new_deter))
+        out = self.stoch_proj2(out)
 
-        mean, std = jnp.split(out, 2, axis=-1)  # (B, S), (B, S)
+        mean, std = jnp.split(out, 2, axis=-1)
         std = nn.softplus(std) + 0.1
         stoch = sample_normal(key, mean, std)
         return State(mean, std, stoch, new_deter)
@@ -76,11 +76,11 @@ class Posterior(eqx.Module):
         )
 
     def __call__(self, obs: Array, prior: State, key: jr.PRNGKey) -> State:
-        inp = jnp.concatenate([prior.deter, obs], axis=-1)  # (B, D + O)
-        hidden = nn.elu(self.post_proj1(inp))  # (B, H)
-        out = self.post_proj2(hidden)  # (B, 2 * S)
+        inp = jnp.concatenate([prior.deter, obs], axis=-1)
+        hidden = nn.elu(self.post_proj1(inp))
+        out = self.post_proj2(hidden)
 
-        mean, std = jnp.split(out, 2, axis=-1)  # (B, S), (B, S)
+        mean, std = jnp.split(out, 2, axis=-1)
         std = nn.softplus(std) + 0.1
         stoch = sample_normal(key, mean, std)
         return State(mean, std, stoch, prior.deter)
@@ -119,47 +119,52 @@ class Model(eqx.Module):
         self.deter_size = deter_size
 
     def __call__(
-        self, obs: Array, state: State, action: Array, key: jr.PRNGKey
+        self, obs: Array, prev_post: State, prev_action: Array, key: jr.PRNGKey
     ) -> Tuple[State, State]:
         keys = jr.split(key, 2)
-        prior = self.prior(state, action, keys[0])
+        prior = self.prior(prev_post, prev_action, keys[0])
         post = self.posterior(obs, prior, keys[1])
         return post, prior
 
     def rollout_prior(
-        self, init_state: State, action_seq: Array, key: jr.PRNGKey
+        self, init_post: State, action_seq: Array, key: jr.PRNGKey
     ) -> State:
-        def step(state, step):
-            key, action = step
-            new_state = self.prior(state, action, key)
-            return new_state, new_state
+        def step(prev_prior, step):
+            key, prev_action = step
+            new_prior = self.prior(prev_prior, prev_action, key)
+            return new_prior, new_prior
 
         T, _ = action_seq.shape
         keys = jr.split(key, T)
-        _, states = lax.scan(step, init_state, (keys, action_seq))
+        _, states = lax.scan(step, init_post, (keys, action_seq))
         return states
 
     def rollout_posterior(
-        self, obs_seq: Array, init_state: State, action_seq: Array, key: jr.PRNGKey
+        self, obs_seq: Array, init_post: State, action_seq: Array, key: jr.PRNGKey
     ) -> Tuple[State, State]:
-        def step(state, step):
-            key, obs, action = step
-            post, prior = self(obs, state, action, key)
+        def step(prev_post, step):
+            key, obs, prev_action = step
+            post, prior = self(obs, prev_post, prev_action, key)
             return post, (post, prior)
 
         T, _ = action_seq.shape
         keys = jr.split(key, T)
         _, (post_seq, prior_seq) = lax.scan(
-            step, init_state, (keys, obs_seq, action_seq)
+            step, init_post, (keys, obs_seq, action_seq)
         )
         return post_seq, prior_seq
 
-    def initial_state(self, B: int) -> State:
+    def decode(self, post: State) -> Array:
+        feature = jnp.concatenate([post.stoch, post.deter], axis=-1)
+        return self.output_proj(feature)
+
+    def init_post(self, B: Optional[int] = None) -> State:
+        B = () if B is None else (B,)
         return State(
-            jnp.zeros((B, self.stoch_size)),
-            jnp.zeros((B, self.stoch_size)),
-            jnp.zeros((B, self.stoch_size)),
-            jnp.zeros((B, self.deter_size)),
+            jnp.zeros(B + (self.stoch_size,)),
+            jnp.ones(B + (self.stoch_size,)),
+            jnp.zeros(B + (self.stoch_size,)),
+            jnp.zeros(B + (self.deter_size,)),
         )
 
 
@@ -167,15 +172,14 @@ def rssm_loss(
     model: Model, obs_seq: Array, action_seq: Array, key: jr.PRNGKey
 ) -> Tuple[Array, Array]:
     B, _, _ = obs_seq.shape
-    init_state = model.initial_state(B)
+    init_post = model.init_post(B)
 
     keys = jr.split(key, B)
     post_seq, prior_seq = vmap(model.rollout_posterior)(
-        obs_seq, init_state, action_seq, keys
+        obs_seq, init_post, action_seq, keys
     )
 
-    feature_seq = jnp.concatenate([post_seq.stoch, post_seq.deter], axis=-1)
-    out_seq = vmap(vmap(model.output_proj))(feature_seq)
+    out_seq = vmap(vmap(model.decode))(post_seq)
 
     obs_loss = mse_loss(model, out_seq, obs_seq)
     kl_loss = kl_divergence(post_seq, prior_seq).mean()
