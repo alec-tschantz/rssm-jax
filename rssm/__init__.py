e268ed5bef54c6064deb6abafdf6ee519b2bff88
Alexander Tschantz <tschantz.alec@gmail.com>
2025-02-26 11:32:59 +0000

    v3

--- rssm/__init__.py
+++ rssm/__init__.py
@@ -1,11 +1,1 @@
-from .model import (
-    init_post_state,
-    forward_encoder,
-    forward_decoder,
-    forward_model,
-    rollout,
-    rollout_prior,
-)
-
-from . import utils 
-from . import train
+from .model import Model
--- rssm/model.py
+++ rssm/model.py
@@ -1,160 +1,163 @@
-import optax
+from typing import Tuple, NamedTuple
+
 import equinox as eqx
-from typing import NamedTuple, Tuple
-from jax import Array, numpy as jnp, random as jr, nn, lax, vmap
-
-
-class Prior(eqx.Module):
-    rnn_cell: eqx.nn.GRUCell
-    fc_input: eqx.nn.Linear
-    fc_state: eqx.nn.Linear
-    fc_logits: eqx.nn.Linear
-    norm_input: eqx.nn.RMSNorm
-    norm_state: eqx.nn.RMSNorm
-    num_discrete: int
-    discrete_dim: int
-
-
-class Posterior(eqx.Module):
-    fc_input: eqx.nn.Linear
-    fc_logits: eqx.nn.Linear
-    norm_input: eqx.nn.RMSNorm
-    num_discrete: int
-    discrete_dim: int
-
-
-class Encoder(eqx.Module):
-    fc1: eqx.nn.Linear
-    fc2: eqx.nn.Linear
-    norm1: eqx.nn.RMSNorm
-    norm2: eqx.nn.RMSNorm
-
-
-class Decoder(eqx.Module):
-    fc1: eqx.nn.Linear
-    fc2: eqx.nn.Linear
-    norm1: eqx.nn.RMSNorm
-    norm2: eqx.nn.RMSNorm
-
-
-class Model(eqx.Module):
-    prior: Prior
-    posterior: Posterior
-    encoder: Encoder
-    decoder: Decoder
-    logit_dim: int
-    state_dim: int
+from jax import Array, numpy as jnp, random as jr, nn, lax
 
 
 class State(NamedTuple):
     logits: Array
-    sample: Array
-    state: Array
+    deter: Array
+    stoch: Array
 
 
-def forward_prior(
-    prior: Prior, prev_post: State, action: Array, key: jr.PRNGKey
-) -> State:
-    prev_sample = prev_post.sample.reshape(-1)
-    feat = jnp.concatenate([action, prev_sample], axis=-1)
-    hidden = prior.norm_input(nn.silu(prior.fc_input(feat)))
-    state = prior.rnn_cell(hidden, prev_post.state)
-    hidden = prior.norm_state(nn.silu(prior.fc_state(state)))
-    logits = prior.fc_logits(hidden).reshape(prior.num_discrete, prior.discrete_dim)
-    logits, sample = sample_logits(logits, key)
-    return State(logits, sample, state)
+class Model(eqx.Module):
+    fc_act: eqx.nn.Linear
+    fc_deter: eqx.nn.Linear
+    fc_stoch: eqx.nn.Linear
+    norm_act: eqx.nn.RMSNorm
+    norm_deter: eqx.nn.RMSNorm
+    norm_stoch: eqx.nn.RMSNorm
+    gru_cell: eqx.nn.GRUCell
 
+    norm_prior: eqx.nn.RMSNorm
+    fc_prior: eqx.nn.Linear
+    fc_logits_prior: eqx.nn.Linear
 
-def forward_posterior(
-    post: Posterior, obs_emb: Array, prior_state: State, key: jr.PRNGKey
-) -> State:
-    inp = jnp.concatenate([obs_emb, prior_state.state], axis=-1)
-    hidden = post.norm_input(nn.silu(post.fc_input(inp)))
-    logits = post.fc_logits(hidden).reshape(post.num_discrete, post.discrete_dim)
-    logits, sample = sample_logits(logits, key)
-    return State(logits, sample, prior_state.state)
+    norm_post: eqx.nn.RMSNorm
+    fc_post: eqx.nn.Linear
+    fc_logits_post: eqx.nn.Linear
 
+    deter_dim: int
+    num_discrete: int
+    num_classes: int
 
-def forward_encoder(encoder: Encoder, obs: Array) -> Array:
-    hidden = encoder.norm1(nn.silu(encoder.fc1(obs)))
-    out = encoder.fc2(hidden)
-    out = encoder.norm2(out)
-    return out
+    def __init__(
+        self,
+        embed_dim: int,
+        action_dim: int,
+        deter_dim: int,
+        num_discrete: int,
+        num_classes: int,
+        hidden_dim: int,
+        key: jr.PRNGKey,
+    ):
+        stoch_dim = num_discrete * num_classes
+        keys = jr.split(key, 9)
 
+        """ deterministic model """
+        self.fc_act = eqx.nn.Linear(
+            in_features=action_dim, out_features=hidden_dim, key=keys[2]
+        )
+        self.fc_deter = eqx.nn.Linear(
+            in_features=deter_dim, out_features=hidden_dim, key=keys[0]
+        )
+        self.fc_stoch = eqx.nn.Linear(
+            in_features=stoch_dim, out_features=hidden_dim, key=keys[1]
+        )
 
-def forward_decoder(decoder: Decoder, post: State) -> Array:
-    inp = jnp.concatenate([post.sample.reshape(-1), post.state], axis=-1)
-    hidden = decoder.norm1(nn.silu(decoder.fc1(inp)))
-    out = decoder.fc2(hidden)
-    out = decoder.norm2(out)
-    return out
+        self.norm_act = eqx.nn.RMSNorm(shape=hidden_dim)
+        self.norm_deter = eqx.nn.RMSNorm(shape=hidden_dim)
+        self.norm_stoch = eqx.nn.RMSNorm(shape=hidden_dim)
 
+        self.gru_cell = eqx.nn.GRUCell(
+            input_size=hidden_dim * 3, hidden_size=deter_dim, key=keys[3]
+        )
 
-def forward_model(
-    model: Model, obs_seq: Array, action_seq: Array, key: jr.PRNGKey
-) -> Tuple[Array, State, State]:
-    obs_emb_seq = vmap(lambda o: forward_encoder(model.encoder, o))(obs_seq)
-    init_post = init_post_state(model)
-    post_seq, prior_seq = rollout(
-        model.prior, model.posterior, obs_emb_seq, init_post, action_seq, key
-    )
-    out_seq = vmap(lambda s: forward_decoder(model.decoder, s))(post_seq)
-    return out_seq, post_seq, prior_seq
+        """ prior model """
+        self.fc_prior = eqx.nn.Linear(
+            in_features=deter_dim, out_features=hidden_dim, key=keys[4]
+        )
+        self.fc_logits_prior = eqx.nn.Linear(
+            in_features=hidden_dim, out_features=stoch_dim, key=keys[5]
+        )
+        self.norm_prior = eqx.nn.RMSNorm(shape=hidden_dim)
 
+        """ posterior model """
+        self.fc_post = eqx.nn.Linear(
+            in_features=deter_dim + embed_dim, out_features=hidden_dim, key=keys[6]
+        )
+        self.fc_logits_post = eqx.nn.Linear(
+            in_features=hidden_dim, out_features=stoch_dim, key=keys[7]
+        )
+        self.norm_post = eqx.nn.RMSNorm(shape=hidden_dim)
 
-def rollout(
-    prior: Prior,
-    post: Posterior,
-    obs_emb_seq: Array,
-    init_post: State,
-    action_seq: Array,
-    key: jr.PRNGKey,
-) -> Tuple[Array, Array]:
-    def step(prev_post, step_data):
-        k_, ob_, act_ = step_data
-        keys = jr.split(k_, 2)
-        prior_ = forward_prior(prior, prev_post, act_, keys[0])
-        post_ = forward_posterior(post, ob_, prior_, keys[1])
-        return post_, (post_, prior_)
+        self.deter_dim = deter_dim
+        self.num_discrete = num_discrete
+        self.num_classes = num_classes
 
-    keys = jr.split(key, action_seq.shape[0])
-    final_post, (post_seq, prior_seq) = lax.scan(
-        step, init_post, (keys, obs_emb_seq, action_seq)
-    )
-    return post_seq, prior_seq
+    def __call__(self, deter: Array, stoch: Array, action: Array) -> Array:
+        x0 = nn.silu(self.norm_deter(self.fc_deter(deter)))
+        x1 = nn.silu(self.norm_stoch(self.fc_stoch(stoch)))
+        x2 = nn.silu(self.norm_act(self.fc_act(action)))
+        x = jnp.concatenate([x0, x1, x2], axis=-1)
+        return self.gru_cell(x, deter)
 
+    def prior(self, deter: Array) -> Array:
+        x = nn.silu(self.norm_prior(self.fc_prior(deter)))
+        return self._uniform_logits(self.fc_logits_prior(x))
 
-def rollout_prior(
-    prior: Prior, init_post: State, action_seq: Array, key: jr.PRNGKey
-) -> Array:
-    def step(prev_s, step_data):
-        k_, act_ = step_data
-        new_s = forward_prior(prior, prev_s, act_, k_)
-        return new_s, new_s
+    def posterior(self, deter: Array, embed: Array) -> Array:
+        x = jnp.concatenate([deter, embed], axis=-1)
+        x = nn.silu(self.norm_post(self.fc_post(x)))
+        return self._uniform_logits(self.fc_logits_post(x))
 
-    keys = jr.split(key, action_seq.shape[0])
-    _, states = lax.scan(step, init_post, (keys, action_seq))
-    return states
+    def rollout(
+        self,
+        init_state: Tuple[Array, Array],
+        embed_seq: Array,
+        action_seq: Array,
+        key: jr.PRNGKey,
+    ) -> Tuple[Array, Array, Array]:
+        def step(carry, step_data):
+            stoch, deter = carry
+            k, embed, act = step_data
+            deter = self(deter, stoch, act)
+            prior_logits = self.prior(deter)
+            post_logits = self.posterior(deter, embed)
+            stoch = self._sample_logits(post_logits, k)
+            return (stoch, deter), (post_logits, prior_logits, deter, stoch)
 
+        keys = jr.split(key, action_seq.shape[0])
+        _, outputs = lax.scan(step, init_state, (keys, embed_seq, action_seq))
+        return outputs
 
-# TODO: gumbel softmax
-def sample_logits(
-    logits: Array, key: jr.PRNGKey, unimix: float = 0.01
-) -> Tuple[Array, Array]:
-    probs = nn.softmax(logits, axis=-1)
-    uniform = jnp.ones_like(probs) / probs.shape[-1]
-    probs = (1.0 - unimix) * probs + unimix * uniform
-    dist_logits = jnp.log(probs + 1e-8)
-    sample = jr.categorical(key, dist_logits, axis=-1)
-    onehot = nn.one_hot(sample, probs.shape[-1])
-    st_sample = onehot + (probs - lax.stop_gradient(probs))
-    return dist_logits, st_sample
+    def rollout_prior(
+        self,
+        init_state: Tuple[Array, Array],
+        action_seq: Array,
+        key: jr.PRNGKey,
+    ) -> Tuple[Array, Array, Array]:
+        def step(carry, step_data):
+            stoch, deter = carry
+            k, act = step_data
+            deter = self(deter, stoch, act)
+            prior_logits = self.prior(deter)
+            stoch = self._sample_logits(prior_logits, k)
+            return (stoch, deter), (prior_logits, deter, stoch)
 
+        keys = jr.split(key, action_seq.shape[0])
+        _, outputs = lax.scan(step, init_state, (keys, action_seq))
+        return outputs
 
-def init_post_state(model: Model, batch_shape: tuple = ()) -> State:
-    post = model.posterior
-    return State(
-        logits=jnp.zeros(batch_shape + (post.num_discrete, post.discrete_dim)),
-        sample=jnp.zeros(batch_shape + (post.num_discrete, post.discrete_dim)),
-        state=jnp.zeros(batch_shape + (model.state_dim,)),
-    )
+    def init_state(self, batch_shape: Tuple = ()) -> Tuple[Array, Array]:
+        stoch = jnp.zeros(batch_shape + (self.num_discrete * self.num_classes,))
+        deter = jnp.zeros(batch_shape + (self.deter_dim,))
+        return stoch, deter
+
+    def _uniform_logits(
+        self, logits: Array, uniform_mix: float = 0.01, eps: float = 1e-8
+    ) -> Array:
+        logits = logits.reshape(self.num_discrete, self.num_classes)
+        probs = nn.softmax(logits, axis=-1)
+        uniform = jnp.ones_like(probs) / self.num_classes
+        probs = (1.0 - uniform_mix) * probs + uniform_mix * uniform
+        return jnp.log(probs + eps).reshape(-1)
+
+    def _sample_logits(self, logits: Array, key: jr.PRNGKey) -> Tuple[Array, Array]:
+        logits = logits.reshape(self.num_discrete, self.num_classes)
+        probs = nn.softmax(logits, axis=-1)
+
+        sample = jr.categorical(key, logits, axis=-1)
+        onehot = nn.one_hot(sample, self.num_classes)
+        sample = onehot + (probs - lax.stop_gradient(probs))
+        return sample.reshape(-1)
